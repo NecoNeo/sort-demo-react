aaa2b738239764e7a900e5453db1bffd4f1986ee
NecoNeo <neo2314@qq.com>
2018-05-07 20:39:34 +0800

    first component commit

---
+++ src/components/AlgorithmTypes/AlgorithmTypes.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './AlgorithmTypes.css';
+
+class AlgorithmTypes extends Component {
+  render() {
+    return <div class="AlgorithmTypes">Hello!</div>
+  }
+}
+
+export default AlgorithmTypes;
