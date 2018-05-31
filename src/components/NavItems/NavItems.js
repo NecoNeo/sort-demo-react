695b13d2fac82032b6ce5be130b74595828da5c2
NecoNeo <neo2314@qq.com>
2018-05-31 23:04:46 +0800

    add components

---
+++ src/components/NavItems/NavItems.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './NavItems.scss';
+
+class NavItems extends Component {
+  render() {
+    return (
+      <nav className="nav-items">
+        <div>sort competition</div>
+      </nav>
+    );
+  }
+}
+
+export default NavItems;
---
+++ src/components/SortTask/SortTask.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './SortTask.scss';
+
+class SortTask extends Component {
+  render() {
+    return (
+      <nav className="sort-task">
+        <div>sort task</div>
+      </nav>
+    );
+  }
+}
+
+export default SortTask;
